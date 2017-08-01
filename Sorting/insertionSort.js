784753d50c883abdb5e99990270e87338560e3f6
Rohit Mathew <rohit@rabb.it>
2017-07-31 23:01:11 -0700

    Insertion Sorts

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+	for(let i = 1; i < arr.length; i++) {
+		let temp = arr[i];
+		let j = i - 1;
+		while(j >= 0 && arr[j] > temp) {
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = temp;
+	}
+	return arr;
+}
+
+const myArr = [5,3,1,6,7,4];
+console.log(insertionSort(myArr)); 
+
